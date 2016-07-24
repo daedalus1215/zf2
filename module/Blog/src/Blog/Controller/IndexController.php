5d6c2b53d3707c675f1404c1c3cdcaaebf0c89f3
Larry Adams <ladams1776@yahoo.com>
2016-07-24 10:59:19 -0400

    simple add form started

--- module/Blog/src/Blog/Controller/IndexController.php
+++ module/Blog/src/Blog/Controller/IndexController.php
@@ -4,6 +4,7 @@ namespace Blog\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Blog\Form\Add;
 
 class IndexController extends AbstractActionController
 {
@@ -11,4 +12,20 @@ class IndexController extends AbstractActionController
     {
         return new ViewModel();
     }
-} 
+
+    public function addAction()
+    {
+        $form = new Add();
+
+        if ($this->request->isPost()) {
+            $form->setData($this->request->getPost());
+            /**
+             * @todo Save blog post
+             */
+        }
+
+        return new ViewModel(array(
+            'form' => $form,
+        ));
+    }
+}
---
+++ module/Blog/src/Blog/Form/Add.php
@@ -0,0 +1,54 @@
+<?php
+namespace Blog\Form;
+
+use Zend\Form\Element;
+use Zend\Form\Form;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Add extends Form
+{
+    public function __construct()
+    {
+        parent::__construct('add'); // pass in the name of our form.
+
+        // need an element for each field for our form.
+        $title = new Element\Text('title');
+        $title->setLabel('Title');
+        //can add some - there are better ways of doing this.
+        $title->setAttribute('class', 'form-control');
+
+        $slug = new Element\Text('slug');
+        $slug->setLabel('Slug');
+        $slug->setAttribute('class', 'form-control');
+
+        $content = new Element\Textarea('content');
+        $content->setLabel('Content');
+        $content->setAttribute('class', 'form-control');
+
+        $category = new Element\Select('category');
+        $category->setLabel('Category');
+        $category->setAttribute('class', 'form-control');
+        $category->setValueOptions(array(
+            1 => 'PHP',
+            2 => 'Zend Framework',
+            3 => 'MySQL'
+        ));
+
+        //create submit button.
+        $submit = new Element\Submit('submit');
+        $submit->setValue('Add Post');
+        $submit->setAttribute('class', 'btn btn-primary');
+
+        //add the elements to this form.
+        $this->add($title);
+        $this->add($slug);
+        $this->add($content);
+        $this->add($category);
+        $this->add($submit);
+    }
+}
