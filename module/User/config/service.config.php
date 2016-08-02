bb6560b0a8ea416e79c0e2e243dbf54ebbe86653
Larry Adams <ladams1776@yahoo.com>
2016-08-01 20:51:16 -0400

    Added the service so that we can DI into AddUser InputFilter

--- module/User/config/service.config.php
+++ module/User/config/service.config.php
@@ -1,5 +1,10 @@
 <?php
 
-return [];
+return [
+    'factories' => array(
+        'User\InputFilter\AddUser' => function(\Zend\ServiceManager\ServiceLocatorInterface $serviceLocator) {
+            return new \User\InputFilter\AddUser($serviceLocator->get('Zend\Db\Adapter\Adapter'));
+        },
+    ),
+];
 
-?>
