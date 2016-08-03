182f7f343136a9c61efabf9b89bb597b9492a3d6
Larry Adams <ladams1776@yahoo.com>
2016-08-02 20:21:19 -0400

    passing up

--- zf2learn.sql
+++ zf2learn.sql
@@ -58,7 +58,7 @@ CREATE TABLE `post` (
   PRIMARY KEY (`id`),
   KEY `author_id_idx` (`author_id`),
   CONSTRAINT `id` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=121 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=119 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -109,4 +109,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-07-31 17:05:21
+-- Dump completed on 2016-08-02 20:21:05
