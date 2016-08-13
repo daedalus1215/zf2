c3e0c9d12913cce4c328b875757ff5f9a3c72670
LA <ladams1776@yahoo.com>
2016-08-13 17:18:58 -0400

    working on layout's user stuff

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -17,10 +17,18 @@ return array(
       'dsn'            => 'mysql:dbname=zf2learn;host=localhost',
        'username'      => 'root',
        'password'      => ''
-   ),
+   ),    
     'service_manager' => array(
         'factories' => array(
             'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+            // now all view helpers have this factory available and by extension the authentication Adapter
+            'Zend\Authentication\AuthenticationService' => function(\Zend\ServiceManager\ServiceLocatorAwareInterface $serviceLocator) {
+                /**
+                 * @var \User\Service\UserService $userService
+                 */
+                $userService = $serviceLocator->get('User\Service\UserService');               
+                return $userService->getAuthenticationService();
+            }
       ),
     )
 );
