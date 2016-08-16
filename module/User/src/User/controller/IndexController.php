6583b75e1a8028c12c219f8d7df0af25d96a84a7
Larry Adams <ladams1776@yahoo.com>
2016-08-15 20:31:38 -0400

    logoutAction made

--- module/User/src/User/controller/IndexController.php
+++ module/User/src/User/controller/IndexController.php
@@ -16,32 +16,32 @@ use Zend\View\Model\ViewModel;
  */
 class IndexController extends AbstractActionController
 {
-    
+
     /**
      *
      * @var ServiceManager $serviceManager
      */
     protected $serviceManager;
-    
+
     /**
-     * 
+     *
      * @return ServiceManager
      */
     protected function getServiceManager()
     {
         return $this->serviceManager;
     }
-    
+
     /**
-     * 
+     *
      * @param ServiceManager $serviceManager
      */
-    protected function setServiceManager(ServiceManager $serviceManager) 
+    protected function setServiceManager(ServiceManager $serviceManager)
     {
         $this->serviceManager = $serviceManager;
     }
 
-        
+
     /**
      *
      * @param ServiceManager $serviceManager
@@ -59,68 +59,71 @@ class IndexController extends AbstractActionController
 
         return new ViewModel();
     }
-    
-    public function addAction() 
+
+    public function addAction()
     {
         $form = new Add();
-        
+
         if ($this->request->isPost()) {
             $user = new User();
-            
+
             $form->bind($user);
             $form->setInputFilter($this->getServiceManager()->get('User\InputFilter\AddUser'));
             $form->setData($this->request->getPost());
-            
+
             if ($form->isValid()) {
                 $user->setUserGroup(UserService::GROUP_REGULAR);
                 $this->getUserService()->add($user);
                 $this->flashMessenger()->addSuccessMessage('The user has been added!');
             }
         }
-        
+
         return new ViewModel(array(
             'form' => $form,
         ));
     }
-    
-    public function loginAction() 
+
+    public function loginAction()
     {
         if ($this->identity() != null) {
             $this->flashMessenger()->addErrorMessage('You are already logged in');
             return $this->redirect()->toRoute('home');
         }
-        
+
         $form = new \User\Form\Login();
-        
+
         if ($this->request->isPost()) {
             $form->setData($this->request->getPost());
             $form->setInputFilter(new \User\InputFilter\Login());
-            
+
             if ($form->isValid()) {
                 $data = $form->getData();
                 $loginResult = $this->getUserService()->login($data['email'], $data['password']);
-                
+
                 if ($loginResult) {
                     $this->flashMessenger()->addSuccessMessage('You have been logged in');
                 }
                 else {
                     $this->flashMessenger()->addErrorMessage('Invalid login credentialsl');
                 }
-            }            
+            }
         }
-        
+
         return new ViewModel(['form' => $form]);
     }
-    
-    
-    
+
+    public function logoutAction()
+    {
+        
+    }
+
     /**
-     * 
+     *
      * @return UserService
      */
     protected function getUserService()
     {
         return $this->getServiceManager()->get('User\Service\UserService');
     }
-    
+
 }
