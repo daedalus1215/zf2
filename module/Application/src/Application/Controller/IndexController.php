a1e5e99fe693e113dc26ce85fcdc4abffa2eb8f1
Larry Adams <ladams1776@yahoo.com>
2016-08-15 20:39:59 -0400

    testing session with application indexAction

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -10,12 +10,21 @@
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Session\Container;
 use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+        // The name of the namespace we want to have access to our session container.
+        $namespace = 'user';
+        // Testing session functionality
+        $container = new Container($namespace); // create session container
+        // Add variables to the session container.
+        $container->name = 'Larry Adams';
+        $container->website = 'larry-adams.info';
+
         return new ViewModel();
     }
 }
